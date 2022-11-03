b13082ccfb1222c114303690ca281c94481f0530
HieuND <hieund.bhsoft@gmail.com>
2022-11-03 16:20:45 +0700

    delete unuse decorator

--- server/src/decorators/roles.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { RolesType, ROLES_KEY} from '../shared';
-
-export const Roles = (...roles: RolesType[]) => SetMetadata(ROLES_KEY, roles);
--- server/src/guards/index.ts
+++ server/src/guards/index.ts
@@ -1,1 +1,1 @@
-export * from './roles.guard';
+export * from './jwt-auth.guard';
