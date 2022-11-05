009e5cbb6ccbc497be525c8908f588b93c05e044
Nguyen Dung Hieu <hieund.bhsoft@gmail.com>
2022-11-05 17:10:30 +0700

    export the du thi

--- client/src/components/pdf/TheDuThi.tsx
+++ client/src/components/pdf/TheDuThi.tsx
@@ -1,12 +1,149 @@
-import React from 'react'
+import {Box, Grid, Typography} from '@mui/material';
+import {printDocument} from 'utils/helper';
 
-const TheDuThi = () => {
+const TheDuThi = (props: any) => {
+  const savePdf = () => {
+    const elem = document.getElementById('divToPrint');
+    const opt = {
+      orientation: 'l',
+      unit: 'px',
+    };
+    printDocument(elem, opt);
+  };
+
   return (
-    <div id="divToPrint" >
-      <div>Note: Here the dimensions of div are same as A4</div> 
-      <div>You Can add any component here</div>
-    </div>
-  )
-}
+    <>
+      <button onClick={savePdf}>Save</button>
+      <div id="divToPrint" style={{width: '1122px'}}>
+        <Box sx={{display: 'flex', gap: '200px', paddingTop: '75px', justifyContent: 'center'}}>
+          <Typography variant="h6" fontFamily="serif" gutterBottom>
+            TRƯỜNG ĐẠI HỌC THỦY LỢI
+          </Typography>
+          <Typography variant="h6" fontFamily="serif" gutterBottom>
+            <strong>CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM</strong>
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            marginLeft: '150px',
+            display: 'flex',
+            gap: '300px',
+            justifyContent: 'space-arround',
+          }}
+        >
+          <Typography variant="h6" fontFamily="serif" gutterBottom>
+            <strong>TRUNG TÂM TIN HỌC</strong>
+          </Typography>
+          <Typography variant="h6" fontFamily="serif" gutterBottom>
+            <strong>Độc lập - Tự do - Hạnh phúc</strong>
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: '50px',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h6" fontFamily="serif">
+            <strong>THẺ DỰ THI</strong>
+          </Typography>
+          <Typography variant="h6" fontFamily="serif">
+            <strong>KỲ THI CHUẨN CÔNG NGHỆ THÔNG TIN CƠ BẢN ĐỢT 12</strong>
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            marginTop: '50px',
+            gap: '25px',
+            justifyContent: 'center',
+          }}
+        >
+          <Box
+            sx={{
+              border: '1px solid black',
+              width: '3cm',
+              minWidth: '3cm',
+              height: '4cm',
+              minHeight: '4cm',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Typography variant="body2">Ảnh 3x4</Typography>
+          </Box>
+          <Grid container sx={{width: '800px'}}>
+            <Grid item xs={6}>
+              <Typography sx={{textOverflow: 'ellipsis'}} variant="body2" fontFamily="serif">
+                Phòng thi: {props.phongThi}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body2" fontFamily="serif">
+                SBD: {props.SBD}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body2" fontFamily="serif">
+                Họ và tên: {props.fullname}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body2" fontFamily="serif">
+                Giới tính:{props.gender}
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="body2" fontFamily="serif">
+                Ngày sinh:{props.birthday}
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="body2" fontFamily="serif">
+                Nơi sinh:{props.birthplace}
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="body2" fontFamily="serif">
+                MSV:{props.MSV}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+        <Typography
+          variant="body1"
+          fontWeight={700}
+          marginLeft="30px"
+          marginTop="25px"
+          fontFamily="serif"
+        >
+          Thí sinh mang theo CMND và có mặt tại phòng thi lúc {props.testTime}, ngày{' '}
+          {props.testDate}.
+        </Typography>
+        <Box
+          sx={{
+            marginTop: '30px',
+            width: 'fit-content',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginLeft: '678px',
+          }}
+        >
+          <Typography variant="body2" marginTop="25px" fontFamily="serif">
+            Hà Nội, Ngày {('0' + new Date().getDate()).slice(-2)} Tháng{' '}
+            {('0' + (new Date().getMonth() + 1)).slice(-2)} Năm {new Date().getFullYear()}
+          </Typography>
+          <Typography variant="body1" marginTop="10px" fontFamily="serif">
+            <strong>TRUNG TÂM TIN HỌC ĐẠI HỌC THỦY LỢI</strong>
+          </Typography>
+        </Box>
+      </div>
+    </>
+  );
+};
 
-export default TheDuThi
+export default TheDuThi;
--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,10 +1,10 @@
 import * as jsPDF from 'jspdf';
 import * as html2canvas from 'html2canvas';
 
-export const printDocument = (input) => {
+export const printDocument = (input, opt) => {
   html2canvas(input).then((canvas) => {
     const imgData = canvas.toDataURL('image/png');
-    const pdf = new jsPDF();
+    const pdf = new jsPDF.jsPDF(opt);
     pdf.addImage(imgData, 'JPEG', 0, 0);
     // pdf.output('dataurlnewwindow');
     pdf.save('download.pdf');
